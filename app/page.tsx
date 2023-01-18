6f2b5a72b899ead32b493584f81c26408f7769d2
trueshizus <jesus.herrera.90@gmail.com>
2023-01-18 20:04:21 +0100

    Add Sentence component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
+import Sentence from "@/components/Sentence";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1> Dutch Trainer</h1>
+      <h1>Dutch Trainer</h1>
+      <Sentence />
     </main>
   );
 }
---
+++ components/Sentence.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sentence = () => {
+  return (
+    <div>
+      <span>Start</span>
+      <input type="text" />
+      <span>end</span>
+    </div>
+  );
+};
+
+export default Sentence;
