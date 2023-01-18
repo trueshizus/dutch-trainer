a619166e8b84219d99d9a6324dd17c05d154f27d
trueshizus <jesus.herrera.90@gmail.com>
2023-01-18 20:37:38 +0100

    Change sentence example

--- components/Sentence.tsx
+++ components/Sentence.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+"use client";
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEvent,
+  useState,
+} from "react";
 
 const Sentence = () => {
+  const [result, setResult] = useState("⏳"); // ✅ or ❌ or ⏳
+
+  const handleResponse = (e: FormEvent<HTMLInputElement>) => {
+    switch (e.currentTarget.value) {
+      case "werk":
+        setResult("✅");
+        break;
+      case "":
+        setResult("⏳");
+        break;
+      default:
+        setResult("❌");
+        break;
+    }
+  };
+
   return (
     <div className="text-center">
-      <span>Start</span>
-      <input type="text" />
-      <span>end</span>
+      <span>Ik</span>
+      <input
+        type="text"
+        onBlur={handleResponse}
+        className="border-b-4 order-indigo-500 text-center"
+        placeholder="werken"
+      />
+      <span>op school.</span>
+
+      <span>{result}</span>
     </div>
   );
 };
